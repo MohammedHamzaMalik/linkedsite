ebf68e66325220357bde1f450dc482ab3a8cc34f
MohammedHamzaMalik <m.h.m.i.malik@gmail.com>
2025-03-02 17:13:36 -0500

    updated App.jsx to add the new route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Suspense } from 'react';
 import Home from './components/Home';
 import WebsiteDisplay from './components/WebsiteDisplay';
+import UserWebsites from './components/UserWebsites';
 import LoadingSpinner from './components/common/LoadingSpinner';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/website/:websiteId" element={<WebsiteDisplay />} />
+          <Route path="/my-websites" element={<UserWebsites />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Suspense>
