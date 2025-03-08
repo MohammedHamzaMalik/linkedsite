84aa1298272a4c285cfb701dae33ce9c98ababd3
MohammedHamzaMalik <m.h.m.i.malik@gmail.com>
2025-03-08 01:19:24 -0500

    Fix build configuration and dependencies

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Print Node and npm versions
+echo "Node version: $(node -v)"
+echo "NPM version: $(npm -v)"
+
+# Install root dependencies
 echo "Installing root dependencies..."
-npm install
+npm ci
 
 echo "Moving to frontend directory..."
 cd frontend
 
 echo "Installing frontend dependencies..."
 npm install --legacy-peer-deps
+npm ci
 
 echo "Building frontend..."
 npm run build
