c9d35d1cf0d4b6dc120daf12fdec183ea5658d8c
MohammedHamzaMalik <m.h.m.i.malik@gmail.com>
2025-03-09 00:46:26 -0500

    removed the chrome commands

--- build.sh
+++ build.sh
@@ -1,47 +1,5 @@
 #!/bin/bash
 
-# Install system dependencies for Puppeteer
-echo "Installing Chrome dependencies..."
-apt-get update && apt-get install -y \
-    fonts-liberation \
-    gconf-service \
-    libappindicator1 \
-    libasound2 \
-    libatk1.0-0 \
-    libcairo2 \
-    libcups2 \
-    libfontconfig1 \
-    libgbm-dev \
-    libgdk-pixbuf2.0-0 \
-    libgtk-3-0 \
-    libicu-dev \
-    libjpeg-dev \
-    libnspr4 \
-    libnss3 \
-    libpango-1.0-0 \
-    libpangocairo-1.0-0 \
-    libpng-dev \
-    libx11-6 \
-    libx11-xcb1 \
-    libxcb1 \
-    libxcomposite1 \
-    libxcursor1 \
-    libxdamage1 \
-    libxext6 \
-    libxfixes3 \
-    libxi6 \
-    libxrandr2 \
-    libxrender1 \
-    libxss1 \
-    libxtst6 \
-    xdg-utils
-
-# Install Chrome
-echo "Installing Chrome..."
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-apt-get install -f -y
-
 # Print Node and npm versions
 echo "Node version: $(node -v)"
 echo "NPM version: $(npm -v)"
